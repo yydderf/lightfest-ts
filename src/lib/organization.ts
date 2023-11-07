4db5384d55b79f833eaa9032a02d0d53077123dc
newb1er <taiwanmichael429@gmail.com>
2023-11-07 19:31:31 +0800

    feat: members tabs

---
+++ src/lib/organization.ts
@@ -0,0 +1,100 @@
+export const departments = [
+    {
+        id: 'public-relations',
+        name: '公關',
+        members: [
+            {name: '蔡涵雅'},
+            {name: '劉子齊'},
+            {name: '張宇弘'},
+            {name: '蕭之晴'},
+            {name: '蘇宥竹'},
+        ],
+    },
+    {
+        id: 'social-propaganda',
+        name: '行銷社群',
+        members: [
+            {name: '林曉玫'},
+            {name: '吳逸萱'},
+            {name: '楊宗勳'},
+            {name: '雷翔茲'},
+        ],
+    },
+    {
+        id: 'event-activities',
+        name: '活動',
+        members: [
+            {name: '陳彥穎'},
+            {name: '王子薇'},
+            {name: '王庭蓁'},
+            {name: '潘丞詡'},
+            {name: '章鎮麒'},
+            {name: '錢品臻'},
+            {name: '張淨'},
+        ],
+    },
+    {
+        id: 'project-management',
+        name: '專案管理',
+        members: [
+            {name: '張鈺群'},
+            {name: '蘇家芸'},
+            {name: '李采蓉'},
+            {name: '王梓善'},
+            {name: '蕭宇婕'},
+        ],
+    },
+    {
+        id: 'visual-design',
+        name: '視覺設計',
+        members: [
+            {name: '賴俞安'},
+            {name: '郭譯'},
+            {name: '杜汶芳'},
+            {name: '吳尚澤'},
+        ],
+    },
+    {
+        id: 'general-affairs',
+        name: '總務',
+        members: [
+            {name: '黃婉瑜'},
+            {name: '王紀璿'},
+            {name: '陳夢霙'},
+            {name: '楊雅茗'},
+        ],
+    },
+    {
+        id: 'resource-management',
+        name: '資料管理',
+        members: [{name: '潘丞詡'}],
+    },
+    {
+        id: 'technology',
+        name: '數位技術',
+        members: [
+            {name: '張棋鈞'},
+            {name: '陳品兆'},
+            {name: '張恩愷'},
+            {name: '張峻魁'},
+            {name: '許訓輔'},
+            {name: '簡右群'},
+            {name: '成文瑄'},
+        ],
+    },
+    {
+        id: 'design',
+        name: '作品',
+        members: [
+            {name: '陳筱媛'},
+            {name: '甯宇綸'},
+            {name: '陳以荷'},
+            {name: '黃翊庭'},
+            {name: '林亞萱'},
+            {name: '梁芸瑄'},
+            {name: '趙雅婷'},
+            {name: '張瀚勻'},
+            {name: '許佳穎'},
+        ],
+    },
+];
